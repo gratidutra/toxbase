726854279c6ee39ccc27289df073e4939680f75e
gratidutra <gratirodrigues.gdr@gmail.com>
2025-01-28 14:54:53 -0300

    login

--- main.py
+++ main.py
@@ -1,27 +1,19 @@
-import yaml
+import toml
 import streamlit as st
-from yaml.loader import SafeLoader
+#from toml.loader import SafeLoader
 import streamlit_authenticator as stauth
-from streamlit_authenticator.utilities import (CredentialsError,
-                                               ForgotError,
-                                               Hasher,
-                                               LoginError,
-                                               RegisterError,
-                                               ResetError,
-                                               UpdateError)
+from streamlit_authenticator.utilities import LoginError 
 import extractor_page
 
-# Loading config file
-with open('config.yaml', 'r', encoding='utf-8') as file:
-    config = yaml.load(file, Loader=SafeLoader)
+config = toml.load('config.toml')
 
-# Creating the authenticator object
+    # Criando o autenticador
 authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days']
-)
+        config['credentials'],
+        config['cookie']['name'],
+        config['cookie']['key'],
+        config['cookie']['expiry_days']
+    )
 
 # Creating a login widget
 try:
