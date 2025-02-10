d4762bf7b99f440a563d109b6c19d13e7f7f033a
gratidutra <gratirodrigues.gdr@gmail.com>
2025-02-10 19:55:15 -0300

    instanced db as postgress

--- app.py
+++ app.py
@@ -1,26 +1,74 @@
-from flask import Flask, render_template, request, flash, Response
-import pandas as pd
-from extractors.extractor import extract_data  # Importa as funções de extração
+from flask import Flask, render_template, request, flash
+from src.database import connection_db
+from extractors.extractor import extract_data
+
 
 app = Flask(__name__)
-app.secret_key = "supersecretkey"
+app.secret_key = "supersecretkey"  # Para mensagens flash no Flask
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    results = None  # Para armazenar os DataFrames convertidos
+    results = None  
 
     if request.method == "POST":
         cas_numbers = request.form.get("cas_numbers")
-        databases = request.form.getlist("databases")  # Obtém lista de bancos selecionados
+        databases = request.form.getlist("databases")  
 
         if not cas_numbers:
-            flash("Please enter toxin IDs in the text area above.", "danger")
+            flash("Por favor, insira um número CAS.", "danger")
         else:
-            # Chama a função de extração, que retorna um dicionário de DataFrames
             df_results = extract_data(cas_numbers, databases)
 
-            # Converte os DataFrames para HTML para exibição na página
-            results = {cas: {db: df.to_html(classes='table table-striped') for db, df in dbs.items()} for cas, dbs in df_results.items()}
+            # Conectar ao banco de dados
+            conn, cursor = connection_db()
+            if conn is None or cursor is None:
+                flash("Erro ao conectar ao banco de dados.", "danger")
+                return render_template("index.html", results=None)
+
+            try:
+                for cas, dbs in df_results.items():
+                    for db_name, data in dbs.items():
+                        if db_name == "PubChem":
+                            for _, row in data.iterrows():
+                                cursor.execute(
+                                    """
+                                    INSERT INTO pubchem (cas_number, cit, molecular_formula, 
+                                                         synonyms, molecular_weight, dates, 
+                                                         description, created_date, updated_date)
+                                    VALUES (%s, %s, %s, %s, %s, %s,%s, NOW(), NOW())
+                                    """,
+                                    (row['CAS Number'], row['CID'], row['Fórmula Molecular'], 
+                                     row['Sinônimos'], row['Peso Molecular'], row['Datas'], 
+                                     row['Descrição'])
+                                )
+
+                        elif db_name == "ECHA":
+                            for _, row in data.iterrows():
+                                cursor.execute(
+                                    """
+                                    INSERT INTO echa (cas_number, ec, molecular_formula, 
+                                                      haz_classification, about_1, 
+                                                      about_2, consumer_user, created_date, updated_date)
+                                    VALUES (%s, %s, %s, %s, %s, %s, %s, NOW(), NOW())
+                                    """,
+                                    (row['CAS Number'], row['EC'], row['Fórmula Molecular'], 
+                                     row['HAZ Classificação'], row['Sobre 1'], row['Sobre 2'], 
+                                     row['Uso Consumidor'])
+                                )
+
+                conn.commit()
+                flash("Dados armazenados com sucesso!", "success")
+
+            except Exception as e:
+                conn.rollback()
+                flash(f"Erro ao salvar no banco de dados: {e}", "danger")
+
+            finally:
+                cursor.close()
+                conn.close()
+
+            results = {cas: {db_name: data.to_html(classes='table table-striped') for db_name, data in dbs.items()} for cas, dbs in df_results.items()}
 
     return render_template("index.html", results=results)
 
