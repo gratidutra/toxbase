bec76a0b47305dcbeea2ab9504244014fa8b5d58
gratidutra <gratirodrigues.gdr@gmail.com>
2025-02-12 14:31:51 -0300

    fix path browsers

--- extractors/extractor.py
+++ extractors/extractor.py
@@ -13,7 +13,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.webdriver.firefox.options import Options_f
+from selenium.webdriver.firefox.options import Options as Options_f
 from selenium.webdriver.chrome.options import Options
 
 import os
